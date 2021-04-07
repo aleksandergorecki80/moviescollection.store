56e032bb2e7d9818aa024935270e8da5d59b37cf
Aleksander <aleksandergorecki80@gmail.com>
2021-04-08 00:53:06 +0100

    Change port

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.use('/api/movies', films);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = 5000;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 });
 
 
