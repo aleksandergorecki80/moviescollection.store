cbb6cd6568b5032cfebbc56d2a861b3f0d026baa
Aleksander <aleksandergorecki80@gmail.com>
2021-04-07 15:43:27 +0100

    Prepare to deploy

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const mongoose = require('mongoose');
-const helmet = require('helmet');
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-
 dotenv.config();
-const port = process.env.PORT;
-
-const films = require('./routes/films');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-
 
 mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conected to MongoDB..'))
   .catch(err => console.log('Could not connect to Mongo DB...', err));
 
 app.use(express.json());
-app.use(helmet());
+require('./startup/prod')(app);
+const films = require('./routes/films');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 app.use('/api/movies', films);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
